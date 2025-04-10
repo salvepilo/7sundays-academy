7e3aae76d137de38deab88672ec828911e14eae4
andrea.farneti98 <andrea.farneti98@gmail.com>
2025-04-10 23:45:50 +0000

    Update generald

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
             </Head>            
                 <AuthProvider>
                     <Component {...pageProps} />
-                    <ToastProvider />
+                    {/* <ToastProvider /> */}
                 </AuthProvider>
         </>
     );
